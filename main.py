9731c115a1e50aa83435860519081ecf5d8e1de8
patpietryg <patpietryg@gmail.com>
2023-11-13 12:17:23 +0100

    Dodano podstawowe funkcje kalkulatora

---
+++ main.py
@@ -0,0 +1,14 @@
+def dodawanie(a, b):
+    return a + b
+
+def odejmowanie(a, b):
+    return a - b
+
+def mnozenie(a, b):
+    return a * b
+
+def dzielenie(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Błąd: Nie można dzielić przez zero"
